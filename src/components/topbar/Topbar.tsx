7a13b4dea5b289b8692e3d593dcda6dff834cdcd
Kieran White <kieran@bluesquareoffices.com>
2022-01-17 15:24:21 +0000

    Adding lity close

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUndo } from '@fortawesome/pro-light-svg-icons';
+import { faTimes, faUndo } from '@fortawesome/pro-light-svg-icons';
 
 import { useControls } from 'components/controls/Controls';
 
@@ -10,9 +10,18 @@ import s from './Topbar.module.scss';
 
 export const Topbar = ({ destinations }: Props) => {
     const { reset } = useControls();
+    const close = () => {
+        if (typeof window !== 'undefined' && typeof window.lity !== 'undefined') {
+            window.lity.current().close();
+        }
+        return;
+    }
 
     return (
         <div className={s.topbar}>
+            <div onClick={close} className={s.topbar__close}>
+                <FontAwesomeIcon icon={faTimes} />
+            </div>
             <div
                 className={s.topbar__reset}
                 onClick={reset}

68c1f83462ae41f74a9dffde6b1922341985515d
Kieran White <kieran@bluesquareoffices.com>
2022-02-23 15:17:35 +0000

    d

--- src/services/logs.service.ts
+++ src/services/logs.service.ts
@@ -6,21 +6,21 @@ const getIpAddress = async () =>
 export const postLog = async (packageId: string, email: string) => {
     const ipAddress = await getIpAddress();
 
-    if (packageId && ipAddress && email) {
-        return await fetch(
-            'https://recommendation.directus.1stformations.co.uk/items/logs/?fields=*.*.*',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    ip_address: ipAddress,
-                    recommended_package: packageId,
-                    email_address: email,
-                }),
-            }
-        )
-        .then((data) => data.json());
-    }
+    if (!ipAddress && !email) return;
+
+    return await fetch(
+        'https://recommendation.directus.1stformations.co.uk/items/logs/?fields=*.*.*',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                ip_address: ipAddress,
+                recommended_package: packageId,
+                email_address: email,
+            }),
+        }
+    )
+    .then((data) => data.json());
 };
